44e94b82ef755c6175d81ffc0bb211a1822db6cc
Benoît Hubert <benoithubert@gmail.com>
2020-01-09 17:13:22 +0100

    Actually connect to db...

--- connection.js
+++ connection.js
@@ -8,4 +8,13 @@ const connection = mysql.createConnection({
   password: process.env.DB_PASS,
 });
 
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
 module.exports = connection;
